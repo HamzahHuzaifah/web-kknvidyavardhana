9ba9bedd007feb774fefba74e481e61316bb2f3e
HamzahHuzaifah <hamzahhuzaifah1945@gmail.com>
2026-09-16 07:59:30 +0700

    Chore: Hardcode Google Client ID

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 import './index.css'
 import App from './App.jsx'
 
-const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID || 'GANTI_DENGAN_CLIENT_ID_ANDA';
+const GOOGLE_CLIENT_ID = '707874718428-e4q8n1u9g68ogg58ho435lf2406iocvq.apps.googleusercontent.com';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
