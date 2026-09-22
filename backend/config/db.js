ade3221d0cdb005a0447e91c333aad111cc4ecbf
HamzahHuzaifah <hamzahhuzaifah1945@gmail.com>
2026-09-22 20:55:08 +0700

    Update

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,7 +6,7 @@ try { require('dotenv').config(); } catch(e) {}
 
 // Database Connection Pool
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOST || '127.0.0.1',
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || 'db_kkn',
